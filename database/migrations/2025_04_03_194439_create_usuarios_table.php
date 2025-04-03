9ea888c60cbec290c7496d6c1a34719073f1f8a1
MrAnderPy <anderssonrojas56@gmail.com>
2025-04-03 14:49:36 -0500

    first commit

---
+++ database/migrations/2025_04_03_194439_create_usuarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('cedula');
+            $table->string('email');
+            $table->string('password');
+            $table->foreignId('area_id')->references('id')->on('area');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
